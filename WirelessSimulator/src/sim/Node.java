3039e8f8f4f3809866d271408af145ec1ef6a1d6
adug <sdnion@gmail.com>
2014-06-27 22:46:37 +0900

    test ok

--- WirelessSimulator/src/sim/Node.java
+++ WirelessSimulator/src/sim/Node.java
@@ -3,10 +3,11 @@ import java.lang.reflect.Array;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.Callable;
 import java.util.concurrent.CyclicBarrier;
 
 
-public class Node implements Runnable{
+public class Node implements Callable<NodeResult>{
 	final List<Integer> neighborsNodeList;
 	Channel channel;
 	final int id;
@@ -24,11 +25,14 @@ public class Node implements Runnable{
 	}
 	
 	//Test
-	public void run(){
-		for (int time=0;time<100;time++){
+	public NodeResult call(){
+		for (int time=0;time<30;time++){
 			System.out.print(time);
 			if (time==id+10){
 				this.addSignal("Data", 20);
+			}
+			if (time==id+20){
+				this.addSignal("ACK", 20);
 			}
 			try {
 				barrier.await();
@@ -36,5 +40,6 @@ public class Node implements Runnable{
 				e.printStackTrace();
 			}
 		}
+		return new NodeResult();
 	}
 }
--- WirelessSimulator/test/test/NodeTest.java
+++ WirelessSimulator/test/test/NodeTest.java
@@ -2,9 +2,14 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -37,21 +42,26 @@ public class NodeTest {
 				
 			}
 		});
+		ArrayList<Future<NodeResult>> results = new ArrayList<Future<NodeResult>>();
+		ExecutorService es = Executors.newCachedThreadPool();
 
+		
 		nodes[0]=new Node(0, n0, channel,cb);
 		nodes[1]=new Node(1, n1, channel,cb);
 		nodes[2]=new Node(2, n2, channel,cb);
-		Thread[] threads=new Thread[3];
+		
 		for (int i=0;i<3;i++){
-			threads[i]=new Thread(nodes[i]);
-			threads[i].start();
+			results.add(es.submit(nodes[i]));
 		}
-		try {
-			Thread.currentThread().wait(10000000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		for (int i=0;i<3;i++){
+			try {
+				NodeResult nr=results.get(i).get();
+			} catch (InterruptedException | ExecutionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
+
 	}
 
 }
